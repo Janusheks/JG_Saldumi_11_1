cf2d998ca999b4c2a0937c475c16b732f819fbec
Janusheks <gaujenieyis@gmail.com>
2023-02-10 09:03:23 +0200

    Saku otro darbinu

--- JG_Saldumi_11/Form1.Designer.cs
+++ JG_Saldumi_11/Form1.Designer.cs
@@ -35,21 +35,27 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.button1.Location = new System.Drawing.Point(294, 182);
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(286, 182);
             this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.button1.Size = new System.Drawing.Size(237, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "Aprekini";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // daudzums
             // 
+            this.daudzums.BackColor = System.Drawing.Color.Linen;
             this.daudzums.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.daudzums.Location = new System.Drawing.Point(76, 190);
             this.daudzums.Multiline = true;
@@ -60,6 +66,7 @@
             // 
             // daudzums_kg
             // 
+            this.daudzums_kg.BackColor = System.Drawing.Color.Linen;
             this.daudzums_kg.Location = new System.Drawing.Point(578, 190);
             this.daudzums_kg.Multiline = true;
             this.daudzums_kg.Name = "daudzums_kg";
@@ -70,10 +77,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label1.Location = new System.Drawing.Point(29, 152);
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(0, 152);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(320, 20);
+            this.label1.Size = new System.Drawing.Size(364, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Ievadiet cenu par kadu gribat pirkt saldumus";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -81,16 +89,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label2.Location = new System.Drawing.Point(498, 152);
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(476, 152);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(276, 20);
+            this.label2.Size = new System.Drawing.Size(312, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Daudzums, cik saldumus varat nopirkt";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Red;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
             this.label3.Location = new System.Drawing.Point(153, 79);
             this.label3.Name = "label3";
@@ -101,6 +111,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Red;
             this.label4.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(91, 33);
             this.label4.Name = "label4";
@@ -108,11 +119,26 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Saldumu cenas aprekinasana!!";
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(66, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 47);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "NOMAINIET KRASINU EKRANAM";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -137,6 +163,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- JG_Saldumi_11/Form1.cs
+++ JG_Saldumi_11/Form1.cs
@@ -34,15 +34,73 @@ namespace JG_Saldumi_11
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            { 
             double sk1 = Convert.ToDouble(daudzums.Text);
             double sk2 = 9.88;
             double rezultats = sk1 / sk2;
             daudzums_kg.Text = rezultats.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ievadijat ne tos datus!!!");
+                
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+            Button2.BackColor = (Button.BackColor == Color.Green) ? Color.Red : Color.Blue;
+            }
+        }
+    }
+    namespace WindowsFormsApplication2
+    {
+        public partial class Form1 : Form
+        {
+            Button[,] btn = new Button[8, 8];
+
+            public Form1()
+            {
+                InitializeComponent();
+
+                for (int x = 0; x < btn.GetLength(0); x++)
+                {
+                    for (int y = 0; y < btn.GetLength(1); y++)
+                    {
+                        btn[x, y] = new Button();
+                        btn[x, y].SetBounds(40 * x, 40 * y, 40, 40);
+                        btn[x, y].Click += new EventHandler(this.btnEvent_click);
+                        Controls.Add(btn[x, y]);
+                        btn[x, y].BackColor = Color.Black;
+                    }
+                }
+
+                /* 
+                btn.Click += new EventHandler(this.btnEvent_click);
+                btn[x,y].Text = Convert.ToString(x+","+y);
+                Controls.Add(btn);
+                btn[x,y].Click += new EventHandler(this.btnEvent_click);
+                */
+            }
+
+            private void form1_load(object sender, EventArgs e)
+            {
+
+            }
+
+            void btnEvent_click(object sender, EventArgs e)
+            {
+                (Control)sender).BackColor = Color.Red;
+            }
         }
     }
 }
